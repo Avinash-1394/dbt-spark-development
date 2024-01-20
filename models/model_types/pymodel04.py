7856b999514609427dccb9706364a92163e537a0
Avinash Santhanagopalan <43074786+Avinash-1394@users.noreply.github.com>
2024-01-19 22:37:03 -0500

    Add additional models

---
+++ models/model_types/pymodel04.py
@@ -0,0 +1,26 @@
+def model(dbt, spark_session):
+    dbt.config(
+        materialized="table",
+        table_type="iceberg",
+        on_schema_change='append_new_columns',
+        format="parquet",
+        polling_interval=5,  # in seconds
+        timeout=100,  # time out in seconds
+        table_properties={
+            "format-version": "2"
+        },
+        engine_config={
+            "CoordinatorDpuSize": 1,
+            "MaxConcurrentDpus": 4,
+            "DefaultExecutorDpuSize": 1,
+        }
+    )
+
+    import time
+    
+    time.sleep(30)
+    data = [(1, "a"), (2, "b"), (3, "c")]
+    cols = ["num", "alpha"]
+    df = spark_session.createDataFrame(data, cols)
+
+    return df
